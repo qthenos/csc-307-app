e78c756edc6594b8a8aced5d9140f759e8831d17
Paul Nassar <pfnassar@calpoly.edu>
2024-09-25 20:01:14 -0700

    IE1 Complete

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -2,18 +2,21 @@
 import React, { useState } from "react";
 
 
-function handleChange(event) {
-    const { name, value } = event.target;
-    if (name === "job")
-        setPerson({ name: person["name"], job: value });
-    else setPerson({ name: value, job: person["job"] });
-}
-
-function Form() {
+function Form(props) {
     const [person, setPerson] = useState({
         name: "",
         job: ""
     });
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({ name: "", job: "" });
+    }
+    function handleChange(event) {
+        const { name, value } = event.target;
+        if (name === "job")
+            setPerson({ name: person["name"], job: value });
+        else setPerson({ name: value, job: person["job"] });
+    }
     return (
         <form>
             <label htmlFor="name">Name</label>
@@ -32,6 +35,7 @@ function Form() {
                 value={person.job}
                 onChange={handleChange}
             />
+            <input type="button" value="Submit" onClick={submitForm} />
         </form>
     );
 }
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -4,8 +4,26 @@ import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
+
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -14,13 +32,17 @@ function MyApp() {
     setCharacters(updated);
   }
 
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
   return (
     <div className="container">
       <Table
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
-      <Form />
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
