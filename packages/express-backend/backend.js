92873b6ee56c56fae3f76e8339c25b543ccf58d8
Paul Nassar <pfnassar@calpoly.edu>
2024-09-30 11:57:35 -0700

    IE2: through 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,12 +6,46 @@ const port = 8000;
 
 app.use(express.json());
 
+const users = {
+    users_list: [
+        {
+            id: "xyz789",
+            name: "Charlie",
+            job: "Janitor"
+        },
+        {
+            id: "abc123",
+            name: "Mac",
+            job: "Bouncer"
+        },
+        {
+            id: "ppp222",
+            name: "Mac",
+            job: "Professor"
+        },
+        {
+            id: "yat999",
+            name: "Dee",
+            job: "Aspring actress"
+        },
+        {
+            id: "zap555",
+            name: "Dennis",
+            job: "Bartender"
+        }
+    ]
+};
+
+app.get("/users", (req, res) => {
+    res.send(users);
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+    console.log(
+        `Example app listening at http://localhost:${port}`
+    );
 });
